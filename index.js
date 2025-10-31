39540a959d081ed4d3be29db46a9fff4c2fe37a1
vishnu272005@gmail.com <vishnu2720052gmail.com>
2025-10-31 13:16:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var sidenavbar = document.querySelector(".side-navbar")
+
+function shownavbar(){
+    sidenavbar.style.left="0%"
+}
+
+function closenavbar(){
+    sidenavbar.style.left="-60%"
+
+}
